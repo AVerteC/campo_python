33ea0a43ac35f29c5655a13488a3e99a44d25cc2
Alan Do <ado@e1z3r5p10.42.us.org>
2017-09-30 16:20:47 -0700

    leaving 42

--- 4201_if/4201_if.py
+++ 4201_if/4201_if.py
@@ -7,11 +7,16 @@ print("The word starts with the letter m, Please guess a five letter word.")
 for x in range(0,11):
 
     guess = input('Your guess: ')
-    except
 
-    if len(guess) < 4:
+
+    if len(guess) < 4 and :
         print("0, 1, 2, 3, 4 that's how we count to five!")
 
-    if len(guess) == 0
+    if len(guess) == 0:
+        print("You wasted a guess =P")
 
-    print(guess[:1])
+    if guess[:1] !='m' and len(guess) == 5:
+        print("abcdefghijklmnopqrstuvwxyz")
+
+    if guess[:1] =='m' and len(guess) == 5 and guess != "memes":
+        print("You have" x "guesses left")
