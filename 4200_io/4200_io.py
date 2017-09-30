fd1a53c34651a7212192f716b388e1ef751e0f65
Alan Do <ado@e1z3r5p10.42.us.org>
2017-09-30 16:00:59 -0700

    excercise2 started

--- 4200_io/4200_io.py
+++ 4200_io/4200_io.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #mad libs program usin argv !
 #use python3
 import sys
---
+++ 4201_if/4201_if.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+secret = "memes"
+
+print("The word starts with the letter m, Please guess a five letter word.")
+
+for x in range(0,11):
+
+    guess = input('Your guess: ')
+    except
+
+    if len(guess) < 4:
+        print("0, 1, 2, 3, 4 that's how we count to five!")
+
+    if len(guess) == 0
+
+    print(guess[:1])
