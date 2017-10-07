f0a0d0e23e11a328bdb743aafee9516108a67675
Alan Do <ado@e1z3r5p10.42.us.org>
2017-10-07 13:33:07 -0700

    finished 4201_if

--- 4201_if/4201_if.py
+++ 4201_if/4201_if.py
@@ -2,21 +2,40 @@
 
 secret = "memes"
 
-print("The word starts with the letter m, Please guess a five letter word.")
+print("The secret word begins with a D.")
+gueso = 0
+
 
 for x in range(0,11):
 
-    guess = input('Your guess: ')
+    guess = input('GUESS: ')
+    print (len(guess))
+    print ("Debuga {} guess".format(gueso))
 
 
-    if len(guess) < 4 and :
+    if len(guess) != 0 & len(guess) < 5   :
         print("0, 1, 2, 3, 4 that's how we count to five!")
+        gueso += 1
+
+
+
+
 
     if len(guess) == 0:
         print("You wasted a guess =P")
+        gueso += 1
+
 
     if guess[:1] !='m' and len(guess) == 5:
-        print("abcdefghijklmnopqrstuvwxyz")
+        print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        gueso += 1
+
 
     if guess[:1] =='m' and len(guess) == 5 and guess != "memes":
-        print("You have" x "guesses left")
+        print("You have {} guesses left".format(gueso))
+        gueso += 1
+
+
+    if guess == "memes":
+        print("Good Job! You are one with the Source.")
+        quit()
