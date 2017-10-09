7f8efe72f32dfca92c9abc2ff95f025c816f9a55
Alan Chen <alan.r.chen@gmail.com>
2017-10-09 10:21:18 -0700

    ewl

---
+++ 4203_num/4203_num.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+
+quotient = float(sys.argv[2])/float(sys.argv[1])
+remainder = int(sys.argv[2]) % int(sys.argv[1])
+
+if (float(sys.argv[2]) % float(sys.argv[1]) )!= 0:
+    remainder = float(remainder)
+
+
+print("{} divided by {} equals {} remainder {}".format(sys.argv[2], sys.argv[1], quotient, remainder))
