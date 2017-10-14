207b1a5019637509c3fbd3948014bd604575d8df
Alan Do <ado@e1z3r5p14.42.us.org>
2017-10-14 16:38:17 -0700

    s

--- 4204_arr/4204_arr.py
+++ 4204_arr/4204_arr.py
@@ -1,23 +1,39 @@
 #!usr/bin/env python3
+from collections import Counter
 import sys
 inputs = sys.argv
 
 inputs.remove('4204_arr.py')
 inputs = list(map(int, inputs))
-
+d1= dict(Counter(inputs))
 list1 = inputs
+print(d1)
+modes = list()
+for k, v in d1.items():
+    if v >= 2:
+        modes.append(k)
+
+print(modes)
+
+'''
+make a counter which pipes out to a list
+then print out the list
+but also exclude when there is no mode
+'''
+
 mean = sum(list1)/len(list1)
-
-
+mathas = len(list1)/2
 
 if (len(list1) % 2 == 0):
     list1.sort()
     mathan = int(len(list1)/2)-1
-    mathas = len(list1)/2
+
     median = (mathas + mathan)/2
 
 else:
     median = mathas
+
+
 
 
 rangee = max(list1)-min(list1)
@@ -33,4 +49,7 @@ print("Min: ", min(list1))
 print("Max: ", max(list1))
 print("Mean: ", mean)
 print("Median: ", median)
+'''
+print("Mode: ", mode)
+'''
 print("Range: ", rangee)
