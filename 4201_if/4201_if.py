18cd2497703a9c8811d4dd1fbd1d5697239ab426
Alan Do <ado@e1z3r5p10.42.us.org>
2017-10-21 12:28:36 -0700

    finishing touches on num and arr

--- 4201_if/4201_if.py
+++ 4201_if/4201_if.py
@@ -9,17 +9,12 @@ gueso = 0
 for x in range(0,11):
 
     guess = input('GUESS: ')
-"""print (len(guess))"""
-    """print ("Debuga {} guess".format(gueso))
-    """
+#print (len(guess))"""
+#print ("Debuga {} guess".format(gueso))
 
     if len(guess) != 0 & len(guess) < 5   :
         print("0, 1, 2, 3, 4 that's how we count to five!")
         gueso += 1
-
-
-
-
 
     if len(guess) == 0:
         print("You wasted a guess =P")
@@ -37,5 +32,5 @@ for x in range(0,11):
 
 
     if guess == "memes":
-        print("Good Job! You are one with the Source.")
-        quit()
+    print("Good Job! You are one with the Source.")
+    quit()
--- 4203_num/4203_num.py
+++ 4203_num/4203_num.py
@@ -1,44 +1,60 @@
 #!/usr/bin/env python3
 import sys
 
+# division segment
+quotient = float(sys.argv[1])/float(sys.argv[2])
+quotient = round(quotient,3)
+remainder = float(sys.argv[1]) % float(sys.argv[2])
+
+if (float(sys.argv[2]) % float(sys.argv[1]) )!= 0:
+    remainder = float(remainder)
+    remainder = round(remainder, 3)
+print("{} divided by {} equals {} remainder {}".format(sys.argv[1], sys.argv[2], quotient, remainder))
+
+#number typechecking/printing system
+
 a = 10
 b = 56.99
 c = 2+3j
 d = 1234
 typ = ""
 
-quotient = float(sys.argv[2])/float(sys.argv[1])
-quotient = round(quotient,4)
-remainder = float(sys.argv[2]) % float(sys.argv[1])
-
-if (float(sys.argv[2]) % float(sys.argv[1]) )!= 0:
-    remainder = float(remainder)
-    remainder = round(remainder, 4)
-print("{} divided by {} equals {} remainder {}".format(sys.argv[2], sys.argv[1], quotient, remainder))
-
-
-
 
 
 def typechecker(var):
     global typ
+    global a
+    global b
+    global c
+    global d
     check1 = (type(var) is int)
     check2 = (type(var) is float)
     check3 = (type(var) is complex)
+
     #long(x) are removed in python3 afaik
     #check4 = (type(var is long))
-
     if check1 == True:
-        typ = typ + "Int"
+        typ = ""
+        typ = typ + "Integer"
         return typ
 
     if check2 == True:
+        typ = ""
         typ = typ + "Float"
         return typ
 
     if check3 == True:
+        typ = ""
         typ = typ + "Complex"
+        var = str(var)
+        var = var.replace("j","i").replace("(","").replace(")","")
+
+
         return typ
+
+
+
+
 
 
 
@@ -57,4 +73,3 @@ typ = ""
 
 typechecker(d)
 print("Variable d contains: {} which is of type: {}".format(d, typ))
-typ = ""
