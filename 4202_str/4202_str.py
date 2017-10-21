e81069da8ee99715108f550ec884b5479afee74b
Alan Do <ado@e1z3r5p10.42.us.org>
2017-10-21 13:34:55 -0700

    more work being done

--- 4202_str/4202_str.py
+++ 4202_str/4202_str.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
-str = "abcdefghijklmnopqrstuvwxyz"
+import sys
+import re
+#inputs = 'abcdefghijklmnopqrstuvwxyz'
 
 
-def iterate_chars(word):
-    for i in range(2, len(word)):
-        print(word[i])
+def eol_cap(s):
+    cap = [False]
+    def repl(m):
+        cap[0] = not cap[0]
+        return m.group(0).upper() if cap[0] else m.group(0).lower()
+    return re.sub(r'[A-Za-z]', repl, s)
 
-iterate_chars(str)
+print(eol_cap(sys.argv[1]))
+
+def eol_noncap(s):
+    cap = [False]
+    def inv_repl(m):
+        cap[0] = not cap[0]
+        return m.group(0).replace(,"*") if m.group(0).upper() else cap[0]
+    return re.sub(r'[A-Za-z]', repl, s)
+
+nexinput = eol_cap(sys.argv[1])
+#def iterate_chars(word):
+#    for i in range(2, len(word)):
+#        print(word[i])
+
+#iterate_chars(str)
