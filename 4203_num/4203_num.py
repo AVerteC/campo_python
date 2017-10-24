266ec8864ed4b67c067c5d216af23f04ef0f80c2
AVerteC <alan.r.chen@gmail.com>
2017-10-23 23:38:34 -0700

    test

--- 4203_num/4203_num.py
+++ 4203_num/4203_num.py
@@ -3,14 +3,15 @@ import sys
 #made by alan chen
 
 # division segment
-quotient = float(sys.argv[1])/float(sys.argv[2])
-quotient = round(quotient,3)
-remainder = float(sys.argv[1]) % float(sys.argv[2])
+def mathing():
+    quotient = float(sys.argv[1])/float(sys.argv[2])
+    quotient = round(quotient,3)
+    remainder = float(sys.argv[1]) % float(sys.argv[2])
 
-if (float(sys.argv[2]) % float(sys.argv[1]) )!= 0:
-    remainder = float(remainder)
-    remainder = round(remainder, 3)
-print("{} divided by {} equals {} remainder {}".format(sys.argv[1], sys.argv[2], quotient, remainder))
+    if (float(sys.argv[2]) % float(sys.argv[1]) )!= 0:
+        remainder = float(remainder)
+        remainder = round(remainder, 3)
+    print("{} divided by {} equals {} remainder {}".format(sys.argv[1], sys.argv[2], quotient, remainder))
 
 #number typechecking/printing system
 
@@ -49,8 +50,6 @@ def typechecker(var):
         typ = typ + "Complex"
         var = str(var)
         var = var.replace("j","i").replace("(","").replace(")","")
-
-
         return typ
 
 
@@ -59,18 +58,39 @@ def typechecker(var):
 
 
 
-typechecker(a)
-print("Variable a contains: {} which is of type: {} ".format(a, typ))
-typ = ""
+def acheck():
+    global typ
+    global b
+    typechecker(a)
+    print("Variable a contains: {} which is of type: {} ".format(a, typ))
+    typ = ""
+
+def bcheck():
+    global typ
+    global b
+    typechecker(b)
+    print("Variable b contains: {} which is of type: {} ".format(b, typ))
+    typ = ""
+
+def ccheck():
+    global typ
+    global c
+    typechecker(c)
+    print("Variable c contains: {} which is of type: {}".format(c, typ))
+    typ = ""
+
+def dcheck():
+    global typ
+    global d
+    typechecker(d)
+    print("Variable d contains: {} which is of type: {}".format(d, typ))
 
 
-typechecker(b)
-print("Variable b contains: {} which is of type: {} ".format(b, typ))
-typ = ""
+def main():
+    mathing()
+    acheck()
+    bcheck()
+    ccheck()
+    dcheck()
 
-typechecker(c)
-print("Variable c contains: {} which is of type: {}".format(c, typ))
-typ = ""
-
-typechecker(d)
-print("Variable d contains: {} which is of type: {}".format(d, typ))
+main()
--- 4204_arr/4204_arr.py
+++ 4204_arr/4204_arr.py
@@ -2,57 +2,79 @@
 #made by alan chen
 from collections import Counter
 import sys
-inputs = sys.argv
 
-inputs.remove('4204_arr.py')
-inputs = list(map(float, inputs))
-inputsmode = list(map(float, inputs))
-d1= dict(Counter(inputsmode))
-list1 = inputs
+def init():
 
-modes = list()
-for k, v in d1.items():
-    if v >= 2:
-        modes.append(k)
+        global d1
+        global inputs
+        global list1
+        #set up inputs
+        inputs = sys.argv
+        inputs.remove('4204_arr.py')
+        inputs = list(map(float, inputs))
+        list1 = inputs
+        inputsmode = list(map(float, inputs))
+        d1= dict(Counter(inputsmode))
 
-
-mode = str(modes)
-marr = [str(a) for a in modes]
-
-'''
-make a counter which pipes out to a list
-then print out the list
-but also exclude when there is no mode
-'''
-
-mean = sum(list1)/len(list1)
-mathas = len(list1)/2
-
-if (len(list1) % 2 == 0):
-    list1.sort()
-    mathan = int(len(list1)/2)-1
-
-    median = (mathas + mathan)/2
-
-else:
-    median = mathas
+def mode():
+    #calculate modes
+        modes = list()
+        for k, v in d1.items():
+            if v >= 2:
+                modes.append(k)
 
 
+        mode = str(modes)
+        marr = [str(a) for a in modes]
+        print('Mode: ', end='')
+        print(", " . join(marr))
+
+#make a counter which pipes out to a list
+#then print out the list
+#but also exclude when there is no mode
+def rangee():
+    #calculate range
+        rangee = max(list1)-min(list1)
+        print("Range: ", round(rangee, 4))
+
+def mean():
+    #calculate mean
+        mean = sum(list1)/len(list1)
+        print("Mean: ", round(mean, 4))
 
 
-rangee = max(list1)-min(list1)
+def median():
+    #calculate the median with indexes, but adjust the calculation if the list is even
+    if (len(inputs) % 2 == 0):
+        inputs.sort()
+        mathas = len(inputs)/2
+        mathan = int(len(inputs)/2)-1
+        median = (mathas + mathan)/2
+    else:
+        mathas = len(inputs)/2
+        median = mathas
 
-"""
-print(sys.argv)
-print(*list, sep='\n')
-"""
+    outs = round(median, 4)
+    print("Median: ", outs )
+
+def minimum():
+        print("Min: ", min(list1))
+
+def maximum():
+        print("Max: ", max(list1))
+
+
+def main():
+    init()
+    minimum()
+    maximum()
+    median()
+    mode()
+    rangee()
 
 
 
-print("Min: ", min(list1))
-print("Max: ", max(list1))
-print("Mean: ", round(mean, 4))
-print("Median: ", round(median, 4))
-print('Mode: ', end='')
-print(", " . join(marr))
-print("Range: ", round(rangee, 4))
+
+
+
+main()
