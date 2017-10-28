e4671f1f39814514135019a30ceb37b79de349ad
AVerteC <alan.r.chen@gmail.com>
2017-10-28 15:08:02 -0700

    continuing hash excercise1

--- 4206_hashs/4206_hash.py
+++ 4206_hashs/4206_hash.py
@@ -2,174 +2,56 @@
 # made by alan chen
 
 def hashmatcher():
-    namehash = {'Aaran':'Guha','Aaryan':'Divate','Aaryan':'Singh'}
-    print(namehash.get('Aaryan'))
+    namehash = {'Aaryan':['Divate','Singh'], 'Alan':['Chen','Do'],'Alexander':['Shtov','Sosnov','Xie'],'Brandon':['Darby','Tedja'],'Daniel':['Camarena','Nelson'],'Ian':['Darby','Mcmamara','Yao']}
     for k in namehash:
-        if namehash.count(k) > 1:
-            print("condition1complete")
+        print (k ,end = "")
+        print (namehash.get(k))
 
 
-def htest():
-    nhash = {'Jason': 'Sen','Jason': 'Do','Jason': 'Dan'}
-    print(nhash)
-    print(nhash.get('Jason'))
+
 """"
-Abelino        Espinoza
-Abhishek        Govindarasu
-Abishek        Saokar
-Adarsh        Bulusu
-Akshay        Namburi
-Akshey        Deokule
-Akshitha        Nagaraj
-Alan        Chen
-Alan        Do
-Aleesa        Ivanov
-Alexander        Shtov
-Alexander        Sosnov
-Alexander        Xie
-Alicia        Tsung
-Amar        Waraich
-Andrew        Chen
-Anmol        Kapoor
-Armaan        Kapoor
-Avi        Mehra
-Brandon        Darby
-Brandon        Tedja
-Brayden        Dinapoli
-Brian        Ma
-Calvin        Lim
-Cesar        Rico
-Chasya        Oba
-Chinmay        Bansal
-Chloe        Chan
-Clarisse        Aruino
-Cole        Miller
-Connor        Sinclair
-Daniel        Camarena
-Daniel        Nelson
-Dennis        Cao
-Denton        Tuiasosopo
-Divya        Prakash
-Eileen        Lin
-Eliot        Vanheumen
-Elliot        Tregoning
-Emily        Ng
-Essam        Sleiman
-Ethan        Dinapoli
-Ezekiel        Liu
-Faizaan        Shaikh
-Gaetan        Juvin
-Giacomo        Guiulfo
-Hannes        Ojala
-Helen        Deng
-Heli        Champaneria
-Ian        Darby
-Ian        Mcnamara
-Ian        Yao
+
+
+
+
 Isaac        Chang
 Isaac        Li
-Iuliia        Iliuk
-Jackie        Chiang
+
 Jacob        Culaton
 Jacob        Zhang
-Jake        Delgado
-Jared        Weinstein
-Jasmin        Anguiano
-Jason        Tedja
+
 Jay        Nathan
+
 Jay        Vishwarupe
-Joe        Krause
-John        Do
-Jonathan        Man
-Joshanne        Chiang
+
 Julia        Garrido
 Julia        Kononova
+
 Justin        Chow
 Justin        Qiu
-Kai        Drumm
+
 Kent        Williams
 Kent        Wong
+
 Kevin        Arackaparambil
 Kevin        Wang
 Kevin        Zhang
-Laine        Miller
-Linnea_pari        Leaver
-Luca        Herden
-Madison        Avila
-Mahad        Abdulkarim
-Malcolm        Alvarez
-Malvee        Vasan
-Mamadi        Diawara
-Manar        Mohamed
-Manjari        Ganesan
-Manthan        Shah
-Matthew        Culaton
-Meo        Leung
-Michelle        Lim
-Mihir        Tyagi
-Mingyun        Kim
-Mira        Kapadia
-Mohamed        Mohamed
-Murari        Ganesan
-Neil        Chulani
-Nhi        Ho
-Nickolas        Mayfield
-Nyalie        Sinclair
-Odin        Eriksen
-Ola        Alsaedi
-Om        Kudtarkar
-Paul        Truong
+
 Peter        Le
 Peter        Nguyen
-Pierce        Mcgoran
+
 Pranav        Damal
 Pranav        Singh
-Pratyush        Vasan
-Preethi        Merugumala
-Pruthvi        Innamuri
-Qiqi        Liang
-Rahul        Champaneria
-Randy        Sun
-Raynay        Ramirez
-Rayyan        Shaikh
+
 Rohan        Taitriya
 Rohan        Tibrewal
-Saanvi        Shreesha
-Sahil        Sandhu
+
 Samuel        Collet
 Samuel        Vu
-Santiago        Valdez
-Sasha        Kononova
-Saumil        Thakore
-Sean        Jones
-Sergei        Litovchenko
-Shayan        Panjwani
-Shivani        Ahuja
-Shriram        Rajasekar
-Somya        Sharma
-Sonali        Bhattacharjee
+
 Sriram        Nagasuri
 Sriram        Selvakumar
-Sungum        Sogani
-Thomas        Ojala
-Timothy        Brand
-Tony        Hendrick
-Trent        Ikehara
-Trina        Nguyen
-Tyler        Ng
-Uma        Shankar
-Vansh        Venugopal
-Vidushika        Vij
-Vindhya        Shigehalli
-Vineet        Sunkavalli
-Vivian        Phung
-Vjai        Thiara
-Vyoma        Raman
-Warren        Fung
-William        Brand
-Willie        Jiang
-Yash        Vij
-Zachary        Hallera
+
 """
 
 #list of first names that should match
@@ -186,6 +68,6 @@ Zachary        Hallera
 
 
 def main():
-    # hashmatcher()
-    htest()
+    hashmatcher()
+
 main()
