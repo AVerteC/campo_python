186a0b76e80d4e77f58688b4341843203f8f159d
AVerteC <alan.r.chen@gmail.com>
2017-10-28 00:50:33 -0700

    Added practice1 and practice2 to finished excercises

---
+++ 4200_io/practice1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#made by alan chen
+
+#use python3
+
+
+
+def practiceinput():
+        addon = "ahah"
+        word = input("Please input a word: ")
+        print(word + " {0}".format(addon))
+
+
+
+
+def main():
+    practiceinput()
+
+
+
+main()
