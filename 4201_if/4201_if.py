cd5b899eefa70bb3e8278e86920231ab1244d285
AVerteC <alan.r.chen@gmail.com>
2017-10-28 00:12:32 -0700

    Finished fixing up 4200_io.py and 4201_if.py

--- 4201_if/4201_if.py
+++ 4201_if/4201_if.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-#made by alan chen
+# made by alan chen
 
 def guessinggame():
-    secret = "memes"
 
     print("The secret word begins with a m.")
     gueso = 0
 
-
-    for x in range(0,11):
+    for x in range(0, 10):
 
         guess = input('GUESS: ')
-    #print (len(guess))"""
-    #print ("Debuga {} guess".format(gueso))
+        # print (len(guess))"""
+        # print ("Debuga {} guess".format(gueso))
 
-        if len(guess) != 0 & len(guess) < 5   :
+        if len(guess) > 0 and < 5:
+            print("0, 1, 2, 3, 4 that's how we count to five!")
+            gueso += 1
+
+        if len(guess) > 5:
             print("0, 1, 2, 3, 4 that's how we count to five!")
             gueso += 1
 
@@ -22,16 +24,13 @@ def guessinggame():
             print("You wasted a guess =P")
             gueso += 1
 
-
-        if guess[:1] !='m' and len(guess) == 5:
+        if guess[:1] != 'm' and len(guess) == 5:
             print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
             gueso += 1
 
-
-        if guess[:1] =='m' and len(guess) == 5 and guess != "memes":
+        if guess[:1] == 'm' and len(guess) == 5 and guess != "memes":
             print("You have {} guesses left".format(gueso))
             gueso += 1
-
 
         if guess == "memes":
             print("Good Job! You are one with the Source.")
@@ -40,6 +39,7 @@ def guessinggame():
 
 def main():
     guessinggame()
+
 
 
 main()
