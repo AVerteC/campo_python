14478aa4a492a40d327a5cb42912049959697d46
AVerteC <alan.r.chen@gmail.com>
2017-11-04 16:48:07 -0700

    4206_hash dict creation success.

--- information_libre/4206_hashs/4206_hash.py
+++ information_libre/4206_hashs/4206_hash.py
@@ -2,66 +2,21 @@
 # made by alan chen
 
 def hashmatcher():
-    namehash = {'Aaryan':['Divate','Singh'], 'Alan':['Chen','Do'],'Alexander':['Shtov','Sosnov','Xie'],'Brandon':['Darby','Tedja'],'Daniel':['Camarena','Nelson'],'Ian':['Darby','Mcmamara','Yao']}
-    for k in namehash:
-<<<<<<< HEAD
-        print (k ,end = "")
-        print (namehash.get(k))
+    file = open("names.txt", "r")
+    allLines = file.readlines()
+    nameDict = {}
+    for element in allLines:
+        nameCombine = element.split()
+        #nameDict[nameCombine[0]]
+        #for item in nameCombine:
+        if nameCombine[0] not in nameDict:
+            nameDict[nameCombine[0]] = [nameCombine[1]]
+        else:
+            nameDict[nameCombine[0]].append(nameCombine[1])
 
-=======
-        if namehash.count(k) > 1:
-            print("condition1complete")
->>>>>>> e1fd86a... continuing hash excercise
-
-
-def htest():
-    nhash = {'Jason': 'Sen','Jason': 'Do','Jason': 'Dan'}
-    print(nhash)
-    print(nhash.get('Jason'))
-""""
+    print(nameDict)
 
 
-
-
-Isaac        Chang
-Isaac        Li
-
-Jacob        Culaton
-Jacob        Zhang
-
-Jay        Nathan
-
-Jay        Vishwarupe
-
-Julia        Garrido
-Julia        Kononova
-
-Justin        Chow
-Justin        Qiu
-
-Kent        Williams
-Kent        Wong
-
-Kevin        Arackaparambil
-Kevin        Wang
-Kevin        Zhang
-
-Peter        Le
-Peter        Nguyen
-
-Pranav        Damal
-Pranav        Singh
-
-Rohan        Taitriya
-Rohan        Tibrewal
-
-Samuel        Collet
-Samuel        Vu
-
-Sriram        Nagasuri
-Sriram        Selvakumar
-
-"""
 
 #list of first names that should match
 
@@ -76,7 +31,7 @@ Sriram        Selvakumar
 
 
 
-def main():
-    # hashmatcher()
-    htest()
-main()
+#def main():
+#    hashmatcher()
+
+#main()
