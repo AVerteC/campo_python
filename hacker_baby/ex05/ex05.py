4836ee084f734b66550fcfa9a32223009d510084
AVerteC <alan.r.chen@gmail.com>
2017-11-11 14:22:40 -0800

    Finished all functions of ex05

--- hacker_baby/ex05/ex05.py
+++ hacker_baby/ex05/ex05.py
@@ -22,13 +22,21 @@ def uinput(nameDict):
         if str(inputu) in nameDict:
             a = str(nameDict.get(str(inputu)))
             print(a)
+            stat1 = "True1"
+            print(stat1)
         else:
             for v, k in nameDict.items():
                 if k == inputu:
                     print(v)
-
+                    break
+            else:
+                if inputu == 'Done':
+                    exit()
+                else:
+                    print('nil')
     else:
         exit()
+
 
 def main():
     b = fileInterpret(getfileLines())
