f1d479c151718a077290969e6e2ac8449864eb45
AVerteC <alan.r.chen@gmail.com>
2017-11-11 16:22:04 -0800

    Finished all functions of ex06

---
+++ hacker_baby/ex06/ex06.py
@@ -0,0 +1,41 @@
+#!usr/bin/env python3
+# made by alan chen
+
+import sys
+
+
+def gateinput():
+    if len(sys.argv) == 1:
+        print("No input detected")
+        exit()
+    else:
+        if sys.argv[1].isnumeric() is True:
+            if int(sys.argv[1]) < 1:
+                print("\n")
+                exit()
+            else:
+                cdn = int(sys.argv[1])
+                return cdn
+        else:
+            print("\n")
+            exit()
+
+
+def d2b(cdn):
+        if cdn <= 1:
+            print("a" + str(cdn))
+
+            return str(cdn)
+        else:
+            print(str(cdn//2)+" "+str(cdn % 2))
+
+            return d2b(cdn//2)+str(cdn % 2)
+
+def main():
+    b = gateinput()
+    print(b)
+    a = d2b(b)
+    print(a)
+
+
+main()
