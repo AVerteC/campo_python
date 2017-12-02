5bbe5f9dfe362faf29c9f6dcc921fc1d7b72e86b
AVerteC <alan.r.chen@gmail.com>
2017-12-02 14:45:04 -0800

    finished basic crot functions

--- cryptography/rot21.py
+++ cryptography/rot21.py
@@ -1,17 +1,62 @@
 #!/usr/bin/env python3
+# Made by Alan Chen
 
 import sys
 
-def rot21():
-    intext = sys.argv[1:]
-    print(intext)
 
-    def converttext():
-        alpha = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, R, S, T, U, V, ]
+def pipecleaner():
+    inputtext = ' '.join(sys.argv[1:])
+    inputtext = inputtext.replace("[", "")
+    inputtext = inputtext.replace("]", "")
+    inputtext = inputtext.replace("\"", "")
+    inputtext = inputtext.replace("", "")
+    inputtext = inputtext.replace("\\n", "")
+    return inputtext
+
+
+def upperbet(c,rot):
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letterindex = uppercase.index(c) + rot
+    if letterindex > 25:
+        letterindex = letterindex % 26
+        print(uppercase[letterindex], end="")
+
+
+def lowerbet(c,rot):
+    lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z']
+    letterindex = lowercase.index(c) + rot
+    if letterindex > 25:
+        letterindex = letterindex % 26
+        print(lowercase[letterindex], end="")
+        
+def crot(inputtext):
+
+    for c in inputtext:
+        if c.isupper():
+            if c in uppercase:
+                    letterindex = uppercase.index(c) + x
+                    if letterindex > 25:
+                        letterindex = letterindex % 26
+                        print(uppercase[letterindex], end="")
+                    else:
+                        print(uppercase[letterindex], end="")
+        elif c.islower():
+            if c in lowercase:
+                    letterindex = lowercase.index(c) + x
+                    if letterindex > 25:
+                        letterindex = letterindex % 26
+                        print(lowercase[letterindex], end="")
+                    else:
+                        print(lowercase[letterindex], end="")
+        else:
+            print(c, end="")
+
 
 
 def main():
-    rot21()
+    crot(pipecleaner())
 
 
 main()
